362cb180ebe3cf56f5a2a8624fe6d415752002a1
Michelle Wan <mtwan625@users.noreply.github.com>
2019-05-04 18:03:41 -0700

    Initial commit

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target; // target of camera
+    Vector3 velocity = Vector3.zero;
+    public float smoothTime = .2f; // to smooth movement of camera
+
+    public bool enableYMax = false;
+    public float yMax = 0;
+    public bool enableYMin = false;
+    public float yMin = 0;
+
+    public bool enableXMax = false;
+    public float xMax = 0;
+    public bool enableXMin = false;
+    public float xMin = 0;
+
+    void FixedUpdate()
+    {
+        // get target position
+        Vector3 targetPos = target.position;
+
+        // vertical clamping
+        float min = target.position.y;
+        float max = target.position.y;
+        if (enableYMin) min = yMin;
+        else if (enableYMax) max = yMax;
+        targetPos.y = Mathf.Clamp(target.position.y, min, max);
+
+        // horizontal clamping
+        min = target.position.x;
+        max = target.position.x;
+        if (enableXMin) min = xMin;
+        else if (enableXMax) max = xMax;
+        targetPos.x = Mathf.Clamp(target.position.x, min, max);
+
+        // set camera position
+        targetPos.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 30f; // adjustable run speed
+    float horizontalMove = 0f;
+    bool isJumping = false;
+    bool isCrouching = false;
+
+    // get information from input every tick
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch")) isCrouching = true;
+        else if (Input.GetButtonUp("Crouch")) isCrouching = false;
+    }
+
+    void FixedUpdate()
+    {
+        // send movement information to controller
+        controller.Move(horizontalMove * Time.fixedDeltaTime, isCrouching, isJumping);
+        // reset values
+        isJumping = false;
+    }
+
+    // function for OnLand() event from controller
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    // function for OnCrouch event from controller
+    public void OnCrouching(bool crouching)
+    {
+        animator.SetBool("isCrouching", crouching);
+    }
+}
